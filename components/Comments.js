bdd819c835ac1abef89fe1a6108983ea1775535f
loki <lohithgowda6617@gmail.com>
2022-12-10 13:19:09 +0530

    status commit

--- components/Comments.js
+++ components/Comments.js
@@ -52,7 +52,6 @@ const Comments = ({ comment, setComment, addComment, comments, isPostingComment 
           <NoResults text='No Comments Yet! Be First to do add the comment.' />
         )}
       </div>
-      
      {userProfile && <div className='absolute bottom-0 left-0  pb-6 px-2 md:px-10 '>
         <form onSubmit={addComment} className='flex gap-4'>
           <input
