55d14ed7bb95cc2bc0b1cd5c11962c1d879f0e1d
loki <lohithgowda6617@gmail.com>
2022-12-10 13:15:42 +0530

    ... new url committ

--- components/Comments.js
+++ components/Comments.js
@@ -52,6 +52,7 @@ const Comments = ({ comment, setComment, addComment, comments, isPostingComment 
           <NoResults text='No Comments Yet! Be First to do add the comment.' />
         )}
       </div>
+      
      {userProfile && <div className='absolute bottom-0 left-0  pb-6 px-2 md:px-10 '>
         <form onSubmit={addComment} className='flex gap-4'>
           <input
